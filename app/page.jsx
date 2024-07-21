9eeff67a9d056a6033bfbc83b06b61cbabbbe152
Udeh Udochukwu <udochukwugodwin13@gmail.com>
2024-07-21 10:45:21 +0100

    first commit

---
+++ app/page.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './page.css'
+// import logo1 from '../public/pom.png'
+import SideBar from "../Components/sidebar"; 
+
+const page = () => {
+  return (
+    <main>
+      <SideBar />
+    </main>
+  )
+}
+
+export default page
