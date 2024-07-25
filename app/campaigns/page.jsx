162289a1f58493a37a6c7f14caf12479486f297a
PhantomOz <faniogor@gmail.com>
2024-07-25 17:09:11 +0100

    This adds not campaigns display

--- app/campaigns/page.jsx
+++ app/campaigns/page.jsx
@@ -1,7 +1,8 @@
 'use client';
 import React from 'react';
-import { Button,Stack,Box,Wrap,WrapItem, ButtonGroup, HStack, VStack, Heading, Text } from '@chakra-ui/react'
+import { Button,Stack,Box,Wrap,WrapItem, ButtonGroup, HStack, VStack, Heading, Text, Center, Icon } from '@chakra-ui/react'
 import { FiPlus } from 'react-icons/fi';
+import { GiLighthouse } from 'react-icons/gi';
 
 function Campaign() {
     const [hasContent, setHasContent] = React.useState(false);
@@ -19,7 +20,11 @@ function Campaign() {
             Create Campaign
           </Button>
         </HStack>
- 
+        <VStack w={'100%'} height={'70vh'} alignContent={'center'} justifyContent={'center'}>
+          <Icon as={GiLighthouse} fontSize={[300,500]} color={'green.900'}/>
+          <Text fontWeight={700} color={'green.900'}>OOPS!!</Text>
+          <Text fontWeight={700}>You Don&apos;t Have Any Recent Campaigns </Text>
+        </VStack>
     </Box>
   )
 }
